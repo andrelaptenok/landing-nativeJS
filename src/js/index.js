04c9baecd8345c852594c76353b4ffb0165ab056
andrelaptenok <andrelaptenok@gmail.com>
2021-02-23 23:51:35 +0300

    screwed js to the form

--- src/js/index.js
+++ src/js/index.js
@@ -19,70 +19,100 @@ overlay.classList.add('overlay');
 document.body.insertAdjacentElement('beforeend', overlay);
 
 const openMenu = () => {
-    catalog.classList.toggle('open');
-    overlay.classList.toggle('active');
-    btnBurger.classList.toggle('wrapper-hamburger');
+  catalog.classList.toggle('open');
+  overlay.classList.toggle('active');
+  btnBurger.classList.toggle('wrapper-hamburger');
 };
 
 const closeMenu = () => {
-    catalog.classList.toggle('open');
-    overlay.classList.toggle('active');
-    btnBurger.classList.toggle('wrapper-hamburger');
+  catalog.classList.toggle('open');
+  overlay.classList.toggle('active');
+  btnBurger.classList.toggle('wrapper-hamburger');
 };
 
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 document.addEventListener('keydown', (e) => {
-    if (e.code === 'Escape') {
-        closeMenu();
-    }
+  if (e.code === 'Escape') {
+    closeMenu();
+  }
 });
 
 //nav openMenu
 
 menu.forEach((el) => {
-    el.addEventListener('click', () => {
-        menu.forEach((el) => {
-            el.classList.remove('link-active');
-        });
-        el.classList.add('link-active');
+  el.addEventListener('click', () => {
+    menu.forEach((el) => {
+      el.classList.remove('link-active');
     });
+    el.classList.add('link-active');
+  });
 });
 
 window.onscroll = function showScroll() {
-    const aboutOff = about.offsetTop;
-    const pricingOff = pricing.offsetTop;
-    const reviewsOff = about.offsetTop;
-    if (window.pageYOffset > 700) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
-    if (window.pageYOffset > aboutOff + pricingOff + reviewsOff - 800) {
-        menu.forEach((element) => {
-            element.classList.remove('link-active');
-        });
-        menu[4].classList.add('link-active');
-    } else if (window.pageYOffset > aboutOff + pricingOff - 400) {
-        menu.forEach((element) => {
-            element.classList.remove('link-active');
-        });
-        menu[3].classList.add('link-active');
-    } else if (window.pageYOffset > pricingOff - 95) {
-        menu.forEach((element) => {
-            element.classList.remove('link-active');
-        });
-        menu[2].classList.add('link-active');
-    } else if (window.pageYOffset > aboutOff - 105) {
-        menu.forEach((element) => {
-            element.classList.remove('link-active');
-        });
-        menu[1].classList.add('link-active');
-    } else {
-        menu.forEach((element) => {
-            element.classList.remove('link-active');
-        });
-        menu[0].classList.add('link-active');
-    }
+  const aboutOff = about.offsetTop;
+  const pricingOff = pricing.offsetTop;
+  const reviewsOff = about.offsetTop;
+  if (window.pageYOffset > 700) {
+    header.classList.add('navbar-fixed');
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
+  if (window.pageYOffset > aboutOff + pricingOff + reviewsOff - 800) {
+    menu.forEach((element) => {
+      element.classList.remove('link-active');
+    });
+    menu[4].classList.add('link-active');
+  } else if (window.pageYOffset > aboutOff + pricingOff - 400) {
+    menu.forEach((element) => {
+      element.classList.remove('link-active');
+    });
+    menu[3].classList.add('link-active');
+  } else if (window.pageYOffset > pricingOff - 95) {
+    menu.forEach((element) => {
+      element.classList.remove('link-active');
+    });
+    menu[2].classList.add('link-active');
+  } else if (window.pageYOffset > aboutOff - 105) {
+    menu.forEach((element) => {
+      element.classList.remove('link-active');
+    });
+    menu[1].classList.add('link-active');
+  } else {
+    menu.forEach((element) => {
+      element.classList.remove('link-active');
+    });
+    menu[0].classList.add('link-active');
+  }
 };
+//Form signap blocks
+
+let nameSignap = document.getElementById('name-signap');
+let emailSignap = document.getElementById('email-signap');
+const passwordSignap = document.getElementById('password-signap');
+
+passwordSignap.addEventListener('click', (e) => {
+  e.preventDefault();
+  nameSignap.value = '';
+  emailSignap.value = '';
+});
+
+//Form contact block
+
+let nameInput = document.getElementById('name-input');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+const submit = document.getElementById('submit');
+let areaText = document.getElementById('area-text');
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (nameInput.value !== '' && email.value !== '') {
+    confirm('Спасибо за Ваш отклик! Наш специалист в скором времени с вами свяжется.');
+  }
+  nameInput.value = '';
+  email.value = '';
+  subject.value = '';
+  areaText.value = '';
+});
